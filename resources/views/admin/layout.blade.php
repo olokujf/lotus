6bdbe95ce8f7462199e5b7ff4ca60db69ec1d9c0
Bruno Doro <dorojf@hotmail.com>
2018-05-15 09:32:40 -0300

    Alteração do layout do admin

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -8,75 +8,73 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Simple Sidebar - Start Bootstrap Template</title>
+    <title>Shop Item - Start Bootstrap Template</title>
 
     <!-- Bootstrap core CSS -->
     <link href="bootstrap/admin/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="css/admin/simple-sidebar.css" rel="stylesheet">
+    <link href="css/admin/lotus.css" rel="stylesheet">
 
 </head>
 
 <body>
 
-<div id="wrapper">
-
-    <!-- Sidebar -->
-    <div id="sidebar-wrapper">
-        <ul class="sidebar-nav">
-            <li class="sidebar-brand">
-                <a href="#">
-                    Gerenciamento Lotus
-                </a>
-            </li>
-            <li>
-                <a href="#">Dashboard</a>
-            </li>
-            <li>
-                <a href="#">Shortcuts</a>
-            </li>
-            <li>
-                <a href="#">Overview</a>
-            </li>
-            <li>
-                <a href="#">Events</a>
-            </li>
-            <li>
-                <a href="#">About</a>
-            </li>
-            <li>
-                <a href="#">Services</a>
-            </li>
-            <li>
-                <a href="#">Contact</a>
-            </li>
-        </ul>
-    </div>
-    <!-- /#sidebar-wrapper -->
-
-    <!-- Page Content -->
-    <div id="page-content-wrapper">
-        <div class="container-fluid">
-            <a href="#menu-toggle" class="btn btn-secondary" id="menu-toggle">Abrir Menu</a>
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="#">Gerenciamento Lotus</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Change Password</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Logout</a>
+                </li>
+            </ul>
         </div>
     </div>
-    <!-- /#page-content-wrapper -->
+</nav>
+
+<!-- Page Content -->
+<div class="container">
+
+    <div class="row">
+
+        <div class="col-lg-3">
+            <h1 class="my-4">Shop Name</h1>
+            <div class="list-group">
+                <a href="#" class="list-group-item active">Home</a>
+                <a href="#" class="list-group-item">About</a>
+                <a href="#" class="list-group-item">Services</a>
+                <a href="#" class="list-group-item">Portifolio</a>
+                <a href="#" class="list-group-item">Team</a>
+                <a href="#" class="list-group-item">Contact</a>
+            </div>
+        </div>
+        <!-- /.col-lg-3 -->
+
+        <div class="col-lg-9">
+
+            <div>
+
+            </div>
+
+        </div>
+        <!-- /.col-lg-9 -->
+
+    </div>
 
 </div>
-<!-- /#wrapper -->
+<!-- /.container -->
 
 <!-- Bootstrap core JavaScript -->
 <script src="js/admin/jquery.min.js"></script>
 <script src="bootstrap/admin/js/bootstrap.bundle.min.js"></script>
-
-<!-- Menu Toggle Script -->
-<script>
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
-</script>
 
 </body>
 

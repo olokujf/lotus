48784e181168162a939b5f118a80463828039576
Bruno Doro <dorojf@hotmail.com>
2018-05-18 07:19:48 -0300

    foi add o plugin de edição de fotos

--- resources/views/admin/banner/banner.blade.php
+++ resources/views/admin/banner/banner.blade.php
@@ -34,7 +34,7 @@
 
     {{--Modal--}}
     <div class="modal fade" id="bannerModal" role="dialog">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-lg">
 
             {{--Modal Content--}}
             <div class="modal-content">
@@ -46,8 +46,43 @@
                 <div class="modal-body">
                     {{Form::open()}}
 
-                    {{Form::label('titulo', 'Titulo')}}
-                    {{Form::text('titulo', '', ['class' => 'form-control', 'required', 'placeholder' => 'Titulo'])}}
+                    <div class="form-group">
+                        {{Form::label('titulo', 'Titulo')}}
+                        {{Form::text('titulo', '', ['class' => 'form-control', 'required', 'placeholder' => 'Titulo'])}}
+                    </div>
+                    <div class="form-group">
+                        {{Form::label('descricao', 'Descrição')}}
+                        {{Form::text('descricao', '', ['class' => 'form-control', 'required', 'placeholder' => 'Descrição'])}}
+                    </div>
+                    <div class="form-group">
+                        {{Form::checkbox('ativo', 1, null, false, ['class' => 'form-check-input'])}}
+                        {{Form::label('ativo', 'Ativo')}}
+                    </div>
+                    <div class="form-group">
+
+                        <div class="image-editor">
+
+                            <input type="hidden" name="image-data" class="hidden-image-data" />
+                            <input type="file" class="cropit-image-input">
+                            <div class="cropit-preview"></div>
+
+                            <div class="row mgt">
+                                <div class="col-md-2">
+                                    <button type="button" class="btn btn-block rotate-ccw"><i class="fa fa-rotate-left"></i></button>
+                                </div>
+                                <div class="col-md-2">
+                                    <button type="button" class="btn btn-block rotate-cw"><i class="fa fa-rotate-right"></i></button>
+                                </div>
+                                <div class="col-md-8">
+                                    <i class="fa fa-image f-f-img-md"></i>
+                                    <input type="range" class="cropit-image-zoom-input">
+                                    <i class="fa fa-image f-f-img-lg"></i>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
 
                     {{Form::close()}}
                 </div>
@@ -57,11 +92,31 @@
     </div>
 
     <script type="text/javascript">
+
         $(document).ready(function(){
+
             $("#novoBanner").click(function(){
                 $("#bannerModal").modal();
             });
+
         });
+
+        $('.image-editor').cropit({
+
+        });
+
+        $('.rotate-cw').click(function() {
+            $('.image-editor').cropit('rotateCW');
+        });
+        $('.rotate-ccw').click(function() {
+            $('.image-editor').cropit('rotateCCW');
+        });
+
+        $('.export').click(function() {
+            var imageData = $('.image-editor').cropit('export');
+            window.open(imageData);
+        });
+
     </script>
 
 @endsection
--- resources/views/admin/head.blade.php
+++ resources/views/admin/head.blade.php
@@ -8,14 +8,19 @@
 <title>Lotus Soluções Tecnológicas</title>
 
 <!-- Scripts -->
+<script src="js/jquery-3.3.1.js" type="text/javascript"></script>
+{{--<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>--}}
+<script src="js/jquery.cropit.js" type="text/javascript"></script>
 <script src="{{ asset('js/app.js') }}" defer></script>
-<script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
 <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+
 
 <!-- Fonts -->
 <link rel="dns-prefetch" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <!-- Styles -->
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="css/cropit.css" rel="stylesheet">
